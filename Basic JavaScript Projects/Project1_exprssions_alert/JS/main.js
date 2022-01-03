3bfaf354b15a804713221a31a3ee85e5cb798db3
Ben <brimedio@pdx.edu>
2022-01-03 11:59:39 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_exprssions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_exprssions_alert/JS/main.js
@@ -1,3 +1,25 @@
+//Alerts:
 alert("Hello, World!");
 window.alert("Window!");
-document.write("Hello, World!");
+
+//Some strings:
+var my_string = "This is a string.";
+var my_string = my_string.fontcolor("blue");
+document.write(my_string);
+
+//Concatenation:
+var other_string = "Another string".fontcolor("red");
+window.alert(other_string);
+document.write("\nIt\'s a beautiful day!");
+document.write("\nThis is a string. " + "And this is also a string.");
+var conc_str = "\nThis is a " + "concatenated string.\n".fontcolor("green");
+document.write(conc_str);
+
+//Multi-var:
+var num1 = "2".fontcolor("orange"), num2 = "3".fontcolor("gray");
+document.write(num2);
+
+//Math:
+var sum = 2 + 3 * 2;
+document.write(sum);
+
