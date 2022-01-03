41a972e4764a362dac0ae4d22a006525c91e4f10
Ben <brimedio@pdx.edu>
2022-01-03 15:14:47 -0800

    Added Project2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+function clickMe(){
+    alert('I HAVE BEEN CLICKED! Generating text...');
+    generate_par();
+}
+
+function generate_par(){
+    var result = "This paragraph was created by JavaScript!"
+    document.getElementById("the_text").innerHTML = result;
+}
+
+function generate_second(){
+    var result = "moonbeams"
+    document.getElementById("third_line").innerHTML += result;
+}
