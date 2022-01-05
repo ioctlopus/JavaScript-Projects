42dc396730158bb7dfd04fd2464091f6f1e76bf6
Ben <brimedio@pdx.edu>
2022-01-04 20:14:14 -0800

    P7

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -13,3 +13,28 @@ function late_for_work(){
         document.getElementById("late").innerHTML = "You're not late!"
     }
 }
+
+function bank_open(){
+    var dat = new Date();
+    if(dat.getHours() > 9 && dat.getHours() < 17){
+        document.getElementById("bank").innerHTML = "The bank is open.";
+    }
+    else{
+        document.getElementById("bank").innerHTML = "The bank is closed.";
+    }
+}
+
+function Time_function(){
+    var Time = new Date().getHours();
+    var Reply;
+    if(Time < 12 == Time > 0){
+        Reply = "It is morning time!";
+    }
+    else if(Time >= 12 == Time < 18){
+        Reply = "It is afternoon.";
+    }
+    else{
+        Reply = "It is evening time."
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
