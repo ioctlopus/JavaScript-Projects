6fdfff43a50b9d4fb49c51b79965720fe1367800
Ben <brimedio@pdx.edu>
2022-01-17 20:47:14 -0800

    Update JavaScript_2.js

--- JavaScript_2.js
+++ JavaScript_2.js
@@ -1,12 +1,17 @@
 function validateForm() {
     let fi = document.forms["myForm"]["fname"].value;
     let em = document.forms["myForm"]["email"].value;
+    let ag = document.forms["myForm"]["age"].value;
     if(fi == "") {
         alert("Name must be filled out");
         return false;
     }
     if(em == ""){
         alert("Email must be filled out");
+        return false;
+    }
+    if(ag == ""){
+        alert("Age must be filled out");
         return false;
     }
 }
